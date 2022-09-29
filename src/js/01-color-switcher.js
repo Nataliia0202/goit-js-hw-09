ab209089f969c045b39bfeebc9d6319065bab02e
Nataliia Svystula <natashasvistula@gmail.com>
2022-09-29 11:08:41 +0300

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const buttonStartEl = document.querySelector('button[data-start]');
+const buttonStopEl = document.querySelector('button[data-stop]');
+buttonStopEl.disabled = true;
+timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+buttonStartEl.addEventListener('click', onButtonStartEl);
+
+function onButtonStartEl() {
+    buttonStartEl.disabled = true;
+    buttonStopEl.disabled = false;
+
+    timerId = setInterval(() => {
+        document.body.style.background = getRandomHexColor();
+    }, 1000);
+
+}
+
+buttonStopEl.addEventListener('click', onButtonStopEl);
+
+function onButtonStopEl() {
+    clearInterval(timerId);
+    buttonStartEl.disabled = false;
+    buttonStopEl.disabled = true;
+}
